f066a8904661e8cf4a5b11b1e8828ca874593c5a
Yao-Yuan Mao <yymao.astro@gmail.com>
2017-12-28 13:18:44 -0500

    new input data

--- data/prepare_data.py
+++ data/prepare_data.py
@@ -1,16 +1,23 @@
 #!/usr/bin/env python
-
 import csv
 import json
+import bisect
 from collections import OrderedDict, Counter
+import argparse
 import requests
 
-if __name__ == '__main__':
-    url = 'https://docs.google.com/spreadsheets/d/1OhcJ0p6Wa2_Xl4mO2_JwEG06wBXbdGlD0hCfVQomOyY/export?gid=1785255735&format=csv'
-    raw_data = list(csv.reader(requests.get(url).text.splitlines()))
+
+def parse_google_sheet(key, gid=None):
+    url = 'https://docs.google.com/spreadsheets/d/{}/export?format=csv{}'.format(key, '' if gid is None else '&gid={}'.format(gid))
+    return list(csv.reader(requests.get(url).text.splitlines()))
+
+
+def prepare_data_from_table():
+    raw_data = parse_google_sheet('1OhcJ0p6Wa2_Xl4mO2_JwEG06wBXbdGlD0hCfVQomOyY', '1785255735')
 
     nodes = OrderedDict()
     links = {}
+    last_node = this_node = None
     for set_id, row in enumerate(raw_data[1:]):
         for i, node in enumerate(row):
             if node.lower() not in nodes:
@@ -24,13 +31,92 @@ if __name__ == '__main__':
                 links[this_link]['entries'].append(set_id)
             last_node = this_node
 
-    counts = Counter((v['category'] for v in nodes.values()))
-
-    data = {
-        'categories': [{'name': c, 'count': counts[i]} for i, c in enumerate(raw_data[0])],
+    return {
+        'categories': [{'name': c} for c in raw_data[0]],
         'nodes': list(nodes.values()),
         'links': list(links.values()),
     }
 
-    with open('data.json', 'w') as f:
-        json.dump(data, f)
+
+def prepare_data_from_matrix():
+    raw_data = parse_google_sheet('1OhcJ0p6Wa2_Xl4mO2_JwEG06wBXbdGlD0hCfVQomOyY', '1680621779')
+
+    nodes = []
+    nodes_map_y = {}
+
+    cat_indices_x = [i for i, c in enumerate(raw_data[0]) if c and i >= 2]
+    assert cat_indices_x[0] == 2
+    cat_indices_x_max = len(raw_data[1])
+    for i, node in enumerate(raw_data[1]):
+        if i < 2:
+            continue
+        if not node:
+            cat_indices_x_max = i
+            break
+        cat_i = bisect.bisect(cat_indices_x, i) - 1
+        if i == cat_indices_x[cat_i]:
+            index_this = 0
+        nodes.append((cat_i+1, index_this, node))
+        index_this += 1
+
+    cat_indices_y = [i for i, row in enumerate(raw_data) if row[0] and i >= 2]
+    assert cat_indices_y[0] == 2
+    for i, row in enumerate(raw_data):
+        if i < 2:
+            continue
+        node = row[1]
+        if not node:
+            break
+        cat_i = bisect.bisect(cat_indices_y, i) - 1
+        if i == cat_indices_y[cat_i]:
+            index_this = 0
+        this = (cat_i, index_this, node)
+        if cat_i:
+            assert this in nodes, '{} does not match in both axes'.format(this)
+        else:
+            nodes.append(this)
+        nodes_map_y[i] = nodes.index(this)
+        if cat_i:
+            assert nodes_map_y[i] == i - cat_indices_y[1], '{} does not match in both axes'.format(this)
+        index_this += 1
+
+    links = []
+
+    for y, row in enumerate(raw_data):
+        if y < 2:
+            continue
+        for x, content in enumerate(row):
+            if x < 2:
+                continue
+            if x >= cat_indices_x_max:
+                break
+            if not content:
+                continue
+
+            cat_x = bisect.bisect(cat_indices_x, x)
+            cat_y = bisect.bisect(cat_indices_y, y) - 1
+            if cat_y+1 == cat_x:
+                this = (nodes_map_y[y], x-2)
+                if this not in links:
+                    links.append(this)
+
+    categories = [raw_data[2][0]] + [raw_data[0][i] for i in cat_indices_x]
+
+    return {
+        'categories': [{'name': c} for c in categories],
+        'nodes': [dict(zip(('category', 'index', 'name'), n)) for n in nodes],
+        'links': [dict(zip(('left', 'right'), l)) for l in links],
+    }
+
+
+def main(use_table, output):
+    prepare_data_this = prepare_data_from_table if use_table else prepare_data_from_matrix
+    with open(output, 'w') as f:
+        json.dump(prepare_data_this(), f)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-o', metavar='OUTPUT', dest='output', default='data.json')
+    parser.add_argument('-t', dest='use_table', action='store_true')
+    main(**vars(parser.parse_args()))
