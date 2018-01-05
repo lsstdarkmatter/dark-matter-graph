85582a648560adfc3c5e781ba0bcdacbecf4cf86
Yao-Yuan Mao <yymao.astro@gmail.com>
2018-01-04 20:20:11 -0500

    attempt to fix slowness...

--- server.py
+++ server.py
@@ -2,48 +2,74 @@
 from __future__ import print_function
 import os
 import sys
-import subprocess
 import socket
 import webbrowser
 import argparse
 from contextlib import closing
+from multiprocessing import Process
+
+try:
+    from http.server import SimpleHTTPRequestHandler
+    from socketserver import TCPServer
+except ImportError:
+    from SimpleHTTPServer import SimpleHTTPRequestHandler
+    from SocketServer import TCPServer
+
+
+_ADDRESS = '127.0.0.1'
+
+
+class MyHTTPRequestHandler(SimpleHTTPRequestHandler):
+    def address_string(self):
+        return self.client_address[0]
+
 
 def find_open_ports():
     for port in range(8887, 8080, -1):
         with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as s:
             try:
-                s.bind(("", port))
+                s.bind((_ADDRESS, port))
             except socket.error:
                 pass
             else:
                 return port
 
 
+def serve_http(port):
+    with open(os.devnull, "w") as devnull:
+        sys.stderr = devnull
+        try:
+            httpd = TCPServer((_ADDRESS, port), MyHTTPRequestHandler)
+            httpd.serve_forever()
+        finally:
+            sys.stderr = sys.__stderr__
+            httpd.server_close()
+
+
 def main(port=None, browser=True, **kwargs):
     port = port or find_open_ports()
-    url = "http://127.0.0.1:{}".format(port)
-    is_py2 = (sys.version_info[0] == 2)
-    cmd = ['python', '-m', 'SimpleHTTPServer' if is_py2 else 'http.server', '{}'.format(port)]
+    url = 'http://{}:{}'.format(_ADDRESS, port)
+
+    p = Process(target=serve_http, args=(port,))
+    p.start()
+
+    print()
+    print('Server running. Open browser and visit => {}'.format(url))
+    print('To exit, press ctrl + c')
+    print()
+
+    if browser:
+        webbrowser.open(url, new=2, autoraise=True)
 
     try:
-        with open(os.devnull, 'w') as devnull:
-            p = subprocess.Popen(cmd, stdout=devnull, stderr=devnull)
-
-            print()
-            print("Server running. Open browser and visit => {}".format(url))
-            print("To exit, press ctrl + c")
-            print()
-
-            if browser:
-                webbrowser.open(url, new=2, autoraise=True)
-
-            p.wait()
+        p.join()
 
     except (KeyboardInterrupt, SystemExit):
-        print(" Bye!")
+        p.terminate()
+        print(' Bye!')
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--port', metavar='PORT', dest='port', type=int)
     parser.add_argument('--no-browser', dest='browser', action='store_false')
